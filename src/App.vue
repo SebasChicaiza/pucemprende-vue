81b17eed19722577eb985e28861dc2b754abbe82
SebasChicaiza <sebastian.erazo275@gmail.com>
2025-05-26 00:04:10 -0500

    Creacion Estructura

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import './assets/main.css'
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap' // JS de Bootstrap (para tooltips, modals, etc.)
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
