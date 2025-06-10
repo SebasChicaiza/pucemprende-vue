f4c5bd1b3bc4b67aa6fcdde45aea873a3fcc244a
SebasChicaiza <sebastian.erazo275@gmail.com>
2025-06-10 07:56:28 -0500

    Eliminacion del token al hacer logout 2

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -26,7 +26,7 @@ onUnmounted(() => {
 
 function logout() {
   localStorage.removeItem('user')
-  localStorage.removeIte('token')
+  localStorage.removeItem('token')
   router.push('/login') // o usar router.push('/login') si prefieres redireccionar
 }
 function cerrarSesion() {
