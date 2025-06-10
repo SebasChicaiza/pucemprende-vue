394dac809a1ef1c272e026cdfbcf299ef599ba9e
JuanJoNavC <jjnavarrete008@gmail.com>
2025-06-10 03:06:01 -0500

    Loader como Component

--- src/components/Admin/Eventos/ModalCrearEvento.vue
+++ src/components/Admin/Eventos/ModalCrearEvento.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, reactive, computed } from 'vue'
+import Loader from '@/components/LoaderComponent.vue'
 
 const props = defineProps({
   show: Boolean,
@@ -43,7 +44,7 @@ function testLoading() {
   setTimeout(() => {
     loading.value = false;
     console.log('✅ Simulación completada.');
-  }, 10000); // simula 3 segundos de espera
+  }, 10000);
 }
 
 
@@ -256,11 +257,8 @@ async function enviarEvento(data) {
           </div>
 
           <!-- <div class="button-row">
-            <button type="button" class="btn btn-cancel" @click="$emit('close')">
-              <i class="fas fa-angle-left"></i>Volver
-            </button>
             <button class="btn btn-primary" @click="testLoading">
-              Siguiente<i class="fas fa-angle-right"></i>
+              Loader button
             </button>
           </div> -->
 
@@ -289,16 +287,8 @@ async function enviarEvento(data) {
       </div>
 
 
-      <!-- Loader overlay -->
-      <div v-if="loading" class="loading-overlay">
-        <div class="dots-container">
-          <div class="dot"></div>
-          <div class="dot"></div>
-          <div class="dot"></div>
-          <div class="dot"></div>
-          <div class="dot"></div>
-        </div>
-      </div>
+      <!-- Loader  -->
+      <Loader v-if="loading"/>
     </div>
   </div>
 </template>
@@ -575,75 +565,5 @@ async function enviarEvento(data) {
   .checkbox-group {
     flex-direction: column;
   }
-}
-
-
-
-/* Loader */
-.dots-container {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-  width: 100%;
-}
-
-.dot {
-  height: 20px;
-  width: 20px;
-  margin-right: 10px;
-  border-radius: 10px;
-  background-color: #b3d4fc;
-  animation: pulse 1.5s infinite ease-in-out;
-}
-
-.dot:last-child {
-  margin-right: 0;
-}
-
-.dot:nth-child(1) {
-  animation-delay: -0.3s;
-}
-
-.dot:nth-child(2) {
-  animation-delay: -0.1s;
-}
-
-.dot:nth-child(3) {
-  animation-delay: 0.1s;
-}
-
-@keyframes pulse {
-  0% {
-    transform: scale(0.8);
-    background-color: #b3d4fc;
-    box-shadow: 0 0 0 0 rgba(178, 212, 252, 0.7);
-  }
-
-  50% {
-    transform: scale(1.2);
-    background-color: #6793fb;
-    box-shadow: 0 0 0 10px rgba(178, 212, 252, 0);
-  }
-
-  100% {
-    transform: scale(0.8);
-    background-color: #b3d4fc;
-    box-shadow: 0 0 0 0 rgba(178, 212, 252, 0.7);
-  }
-}
-
-.loading-overlay {
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 9999;
-  width: 100vw;
-  height: 100vh;
-  background-color: rgba(0, 0, 0, 0.6);
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  pointer-events: all;
 }
 </style>
