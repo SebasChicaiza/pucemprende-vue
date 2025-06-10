ff3b8eba4fc717a546c3737f736d0449c9d0c74e
SebasChicaiza <sebastian.erazo275@gmail.com>
2025-06-09 21:36:16 -0500

    Correcion rutas netlify

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools()],
+  plugins: [vue(), vueDevTools(), viteStaticCopy({
+    targets: [
+      {
+        src: '_redirects',
+        dest: '.' // Copiar a dist/
+      }
+    ]
+  })
+  ],
   server: {
     port: 3000,
   },
