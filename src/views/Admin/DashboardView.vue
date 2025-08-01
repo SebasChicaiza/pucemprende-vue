de93fc72944b3c9f6c3240ee193ddde829b5968f
JuanJoNavC <jjnavarrete008@gmail.com>
2025-07-31 21:22:19 -0500

    Adaptacion dashboard con power bi

--- src/views/Admin/DashboardView.vue
+++ src/views/Admin/DashboardView.vue
@@ -1,11 +1,11 @@
 <script setup>
-import Sidebar from '@/components/Admin/AdminSidebar.vue';
-import PageHeaderRoute from '@/components/PageHeaderRoute.vue';
-import LoaderComponent from '@/components/LoaderComponent.vue';
+import Sidebar from '@/components/Admin/AdminSidebar.vue'
+import PageHeaderRoute from '@/components/PageHeaderRoute.vue'
+import LoaderComponent from '@/components/LoaderComponent.vue'
 </script>
 
 <template>
-  <LoaderComponent v-if="loading"/>
+  <LoaderComponent v-if="loading" />
   <div class="d-flex" style="height: 100vh; overflow: hidden">
     <Sidebar />
 
@@ -13,7 +13,14 @@ import LoaderComponent from '@/components/LoaderComponent.vue';
       <PageHeaderRoute />
 
       <div class="p-4 overflow-y-scroll flex-grow-1" style="height: calc(100vh - 60px)">
-        <p>Este es el dashboard de admin</p>
+        <iframe
+          title="PUCEMPRENDE_PBI"
+          width="100%"
+          height="100%"
+          src="https://app.powerbi.com/view?r=eyJrIjoiYmNiMTZlMTEtNjhiMS00YjViLThlMzctMjgwMTM5Y2MzM2M1IiwidCI6IjU0NDc0ZmIzLTYzNTktNDBjMS1iNzI2LTVkNTZhN2RkMjk3NiIsImMiOjR9"
+          frameborder="0"
+          allowFullScreen="true"
+        ></iframe>
       </div>
     </div>
   </div>
